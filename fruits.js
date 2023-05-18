c32e3a804f85395573cfe028a40b112e3747eac9
Taarini1 <98458225+Taarini1@users.noreply.github.com>
2023-05-18 08:57:24 +0530

    Add files via upload

--- fruits.js
+++ fruits.js
@@ -1,5 +1,6 @@
 img="";
-Status=""
+Status="";
+objects=[]
 function preload(){
 
 img=loadImage("basket.png");
@@ -26,6 +27,8 @@ function gotResults(error,results){
 if(error){console.log(error)};
 if(results){
     console.log(results);
+    results=objects;
+
 
 
 
@@ -37,4 +40,24 @@ if(results){
 
 function draw(){
     image(img,0,0,750,100);
+    if(Status!=""){
+objectDetector.detect(img,gotResults);
+    for(i=0;i<objects.length;i++){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+
+        document.getElementById("status").innerHTML = "Objects are detected";
+        document.getElementById("obj_detected").innerHTML = "Number of objects detected are - "+ objects.length;
+        fill(r,g,b);
+        stroke(r,g,b);
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+percent+"%",objects[i].x+55,objects[i].y+55);
+        noFill();
+        rect(objects[i].x+55,objects[i].y+55,objects[i].width,objects[i].height);
+        
+    }
+
+    }
 }
+
